69c289231aef6a65fa86eabee6d37bab95d1eda5
mxrsel <marsel14082006@gmail.com>
2024-12-04 23:05:43 +0600

    added components from mui for display inputs

--- cipherFrontend/src/components/MessageCipher/MessageCipher.tsx
+++ cipherFrontend/src/components/MessageCipher/MessageCipher.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import {Button, Input, Typography} from "@mui/material";
+import {ArrowDownward, ArrowUpward} from "@mui/icons-material";
 
 const MessageCipher = () => {
     return (
         <div>
+            <h1>Message Cipher</h1>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
 
+                    <Typography>Message
+                <Input
+                    placeholder="Enter message"
+                />
+                    </Typography>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <Typography>Password
+                <Input
+                    type="password"
+                    placeholder="Enter password"
+                    required
+                />
+                <Button> <ArrowDownward/> </Button>
+                <Button> <ArrowUpward/> </Button>
+                </Typography>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <Typography>Result
+                <Input
+                    placeholder="Result"
+                    readOnly
+                />
+                </Typography>
+            </div>
         </div>
     );
 };
